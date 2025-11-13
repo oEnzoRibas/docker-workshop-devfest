bed05bf07016fd1b2de13389abd8919de94d482a
Enzo Ribas <enzorochaleitedinizribas@gmail.com>
2025-11-13 19:28:06 -0300

    adding projects done in the workshop

--- code/projeto-1/script.sh
+++ code/projeto-1/script.sh
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 echo "Hello World"
+=======
+echo "Hello, devfest"
+>>>>>>> fbc79bc (adding projects done in the workshop)
--- code/projeto-2/src/routes/index.js
+++ code/projeto-2/src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Welcome to the Docker Basics App');
+    res.send('Mudei novamente');
 });
 
 router.get('/about', (req, res) => {
